a93bef2e7c1394bf89c214a320d8a392706d2281
Victor Lavrentev <victor.lavrentev@gmail.com>
2019-01-20 21:21:31 +0200

    add

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/dll.hpp>
+#include <boost/python.hpp>
+#include <boost/filesystem.hpp>
+
+using namespace std;
+namespace py = boost::python;
+
+py::list boost_main(py::object in_list){	
+	boost::filesystem::path pathDLL = "dll.so";
+	function<py::list(py::object)> funcExt;
+	funcExt = boost::dll::import<py::list(py::object)>(pathDLL, "get_list");
+	py::list out_list;
+	out_list = funcExt(in_list);
+	return out_list;
+}
+
+BOOST_PYTHON_MODULE(main)
+{	
+    py::def("boost_main", &boost_main);
+}
